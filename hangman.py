4e17aa0039d7f5670878c01138bd0d8634a5244c
willKip <coldbrushl@gmail.com>
2021-05-12 12:32:21 -0400

    Cleanup Imports
    The importlib library has been removed in favor of direct importing.
    For this, the hangman-ai.py file was refactored to use an underscore (_)
    instead of a dash (-). Python does not import file names with dashes
    properly.
    
    Some code has been cleaned up in the hangman.py file, no changes have
    been made to hangman_ai.py. hangman-tester.py has been deleted, its
    function as a mass tester will be replaced by an option in hangman.py.

--- hangman.py
+++ hangman.py
@@ -1,34 +1,31 @@
 import random
-import importlib
+from hangman_ai import *
 
-numgames = 10
+numgames = 1
 wins = 0
-gamesplayed = 0
+games_played = 0
 guesses = 6
 letters = []
 wordlist = []
-ai_module = None
-init_round = None
-make_guess = None
 
 
 def initgame():
-	global wordlist, gamesplayed, wins
-	gamesplayed = 0
+	global wordlist, games_played, wins
+	games_played = 0
 	wins = 0
-	filein = open("base-words.txt", "r")
-	wordlist = filein.read().split("\n")
-	filein.close();
+	wordbase = open("base-words.txt", "r")
+	wordlist = wordbase.read().split("\n")
+	wordbase.close()
+
 
 def gameround():
-	global wins, gamesplayed	 
+	global wins, games_played
 
 	word = random.choice(wordlist).lower()
-	initword = ""
-	for i in word:
-		initword += "_"
-	
-	init_round(initword)
+
+	initword = "_"*len(word)
+
+	initround(initword)
 	guessed = []
 	wrong = 0
 	won = False
@@ -50,37 +47,29 @@ def gameround():
 		print("Your guesses so far: " + str(guessed))
 		print("Current Puzzle: " + puzzle)
 		print("Remaining Incorrect Guesses:", (guesses - wrong))
-		guess = make_guess(argument)
+		guess = makeguess(argument)
 		if guess not in word:
 			wrong += 1
 		if guess not in guessed:
 			guessed.append(guess)
 	
-	gamesplayed += 1
+	games_played += 1
 	if won:
 		print("You won the round!")
 		wins += 1
 	else:
-		print("You lost the round. The word was " + word);
+		print("You lost the round. The word was " + word)
 		
 	
 def main():
-	global ai_module, init_round, make_guess
-	try:
-		ai_module = importlib.import_module("hangman-ai")
-		init_round = getattr(ai_module, "initround")
-		make_guess = getattr(ai_module, "makeguess")
-	except Exception as err:
-		print("Error loading module and/or functions - check the names?")
-		print("Error message:")
-		print(err)
-		quit()
-
 	initgame()
-	for round in range(numgames):
+
+	for _ in range(numgames):
 		gameround()
-	print("Games Played:", gamesplayed)
+
+	print("Games Played:", games_played)
 	print("Games Won:", wins)
-	
+
+
 if __name__ == "__main__":
 	main()
---
+++ hangman_ai.py
@@ -0,0 +1,105 @@
+# COMP1405Z Hangman Algorithm
+# William Lee
+
+# global variables
+refined_list = []
+wrong_letters = []
+letters = []
+prev_guess = ""
+most_common = ""
+
+# wordlist is all the possible words the game could pick as a puzzle. only initialized at start of program
+wordlist = []
+
+filein = open("base-words.txt", "r")
+wordlist = filein.read().lower().split("\n")
+filein.close()
+
+
+# round initialization, called at the start of each round. initializes global variables
+def initround(string):
+    global refined_list, wrong_letters, letters, prev_guess, most_common
+
+    refined_list = [word for word in wordlist if len(word) == len(string)]
+    wrong_letters.clear()
+    letters = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+    prev_guess = string
+    most_common = ""
+
+
+# returns true if the guess made in the previous turn altered the guess string (was correct)
+# additionally, updates prev_guess with current string, indicating it is the latest correct guess to compare off of
+def was_correct(string):
+    global prev_guess
+
+    if string != prev_guess:
+        prev_guess = string
+        return True
+    else:
+        return False
+
+
+# returns true if the inputted word is a valid hangman guess for string
+def check_word(string, word):
+    is_valid = True
+
+    # if the word has a wrong letter, it cannot be a valid guess
+    for char in word:
+        if char in wrong_letters:
+            is_valid = False
+            break
+
+    # otherwise, run some more checks
+    if is_valid:
+        # initialize list of all letters in the orig string so their indexes can be parsed
+        orig_chars = [char for char in string]
+
+        # first check if char positions of orig line up with compare; disregard 'blank' characters
+        # then check if filled in characters of orig ("correct" characters) are in wrong positions of compare.
+        for j in range(len(word)):
+            if orig_chars[j] != "_" and orig_chars[j] != word[j]:
+                is_valid = False
+                break
+
+            # finds valid positions for given letter, and if the character is outside such a position, word is invalid
+            if word[j] in orig_chars:
+                if j not in [i for i, x in enumerate(orig_chars) if x == word[j]]:
+                    is_valid = False
+                    break
+
+    return is_valid
+
+
+# with given word list, finds the most frequent letter. does not include already guessed letters
+def find_most_freq(list_input):
+    global most_common, letters
+
+    letter_freqs = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
+
+    # increments one count for each letter found in every word, but only if not guessed already
+    for word in list_input:
+        for char in word:
+            if char in letters:
+                letter_freqs[char] += 1
+
+    # remove the most common letter from the letters list, since it will already have been guessed
+    most_common = max(letter_freqs, key=letter_freqs.get)
+    letters.remove(most_common)
+
+    return most_common
+
+
+# makes best guess for hangman based on the refined word list's most common letter
+def makeguess(string):
+    global wrong_letters, refined_list
+
+    # if the last guess was right, refine the list anew with the string (only if the list still needs sorting)
+    # otherwise, add the last guess to wrong_letters list
+    if not was_correct(string):
+        wrong_letters.append(most_common)
+
+    else:
+        if len(refined_list) > 1:
+            refined_list[:] = [word for word in refined_list if check_word(string, word)]
+
+    return find_most_freq(refined_list)
