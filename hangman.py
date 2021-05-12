e50786d48b2f6ad8c56cc6fbc3b878970893076b
willKip <coldbrushl@gmail.com>
2021-05-12 14:10:53 -0400

    Fix 'attempt to delete nonexistent' bug, refactor code
    Reindented hangman.py for 4 spaces.
    Added 'verbose' boolean that will disable print statements
    per turn if set to false. End-of-game statistics will still
    show up.
    
    Changed wordbase parsing method to "with open() as f", and
    made it so words are parsed as lowercase at the point of game
    initialization. This has fixed a bug with find_most_freq()'s
    remove from the letters list in hangman_ai.py.

--- hangman.py
+++ hangman.py
@@ -1,75 +1,80 @@
 import random
 from hangman_ai import *
 
-wordlist = []
 games_played = 0
 wins = 0
-numgames = 10
+wordlist = []
+
+numgames = 30
 guesses = 6
+verbose = False
 
 
 def initgame():
-	global wordlist, games_played, wins
-	games_played = 0
-	wins = 0
-	wordbase = open("base_words.txt", "r")
-	wordlist = wordbase.read().split("\n")
-	wordbase.close()
+    global wordlist
+
+    with open("base_words.txt", "r") as f:
+        wordlist = f.read().lower().split("\n")
 
 
 def gameround():
-	global wins, games_played
+    global wins, games_played
 
-	word = random.choice(wordlist).lower()
-	initword = "_" * len(word)
+    word = random.choice(wordlist)
+    initword = "_" * len(word)
 
-	initround(initword, wordlist)
-	guessed = []
-	wrong = 0
-	won = False
-	while wrong < guesses:
-		puzzle = ""
-		argument = ""
-		correct = 0
-		for char in word:
-			if char in guessed:
-				puzzle += char + " "
-				argument += char
-				correct += 1
-			else:
-				argument += "_"
-				puzzle += "_ "
-		if correct >= len(word):
-			won = True
-			break
-		print("Your guesses so far: " + str(guessed))
-		print("Current Puzzle: " + puzzle)
-		print("Remaining Incorrect Guesses:", (guesses - wrong))
+    initround(initword, wordlist)
+    guessed = []
+    wrong = 0
+    won = False
 
-		guess = makeguess(argument)
-		if guess not in word:
-			wrong += 1
-		if guess not in guessed:
-			guessed.append(guess)
-	
-	games_played += 1
+    while wrong < guesses:
+        puzzle = ""
+        argument = ""
+        correct = 0
+        for char in word:
+            if char in guessed:
+                puzzle += char + " "
+                argument += char
+                correct += 1
+            else:
+                argument += "_"
+                puzzle += "_ "
+        if correct >= len(word):
+            won = True
+            break
 
-	if won:
-		print("You won the round!")
-		wins += 1
-	else:
-		print("You lost the round. The word was " + word)
-		
-	
+        if verbose:
+            print("Your guesses so far: " + str(guessed))
+            print("Current Puzzle: " + puzzle)
+            print("Remaining Incorrect Guesses:", (guesses - wrong))
+
+        guess = makeguess(argument)
+        if guess not in word:
+            wrong += 1
+        if guess not in guessed:
+            guessed.append(guess)
+
+    games_played += 1
+
+    if won:
+        wins += 1
+        if verbose:
+            print("You won the round!")
+    else:
+        if verbose:
+            print("You lost the round. The word was " + word)
+
+
 def main():
-	initgame()
+    initgame()
 
-	for _ in range(numgames):
-		gameround()
+    for _ in range(numgames):
+        gameround()
 
-	print("Games Played: ", games_played)
-	print("Games Won: ", wins)
+    print("Games Played: ", games_played)
+    print("Games Won: ", wins)
 
 
 if __name__ == "__main__":
-	main()
+    main()
